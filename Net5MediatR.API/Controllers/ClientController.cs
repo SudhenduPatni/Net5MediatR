bc608cea712eada4a82c496d05c81e4e0b52f70b
SudhenduPatni <sudhendu.patni@hotmail.co.uk>
2021-08-07 10:43:11 +0530

    Add project files.

---
+++ Net5MediatR.API/Controllers/ClientController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Net5MediatR.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientController : ControllerBase
+    {
+        private readonly ILogger<ClientController> _logger;
+
+        public ClientController(ILogger<ClientController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
